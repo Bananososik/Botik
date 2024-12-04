7b2eebf04be0245059597128080c5e75bb206593
Низовцев Иван Данилович <bana100632@gmail.com>
2024-12-04 19:42:21 +1000

    v0.2

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ app = Client("my_bot", api_id = api_id, api_hash = api_hash, bot_token=bot_token
 @app.on_message(filters.command("start"))
 def start(client, message):
     message.reply("Введите количество ваших хромосом: ")
-    if message.text == "46":
-        message.reply("Поздравляю, вы человек!")
-    else:
-        message.reply("Вы даун!")
+    @app.on_message(filters.private)
+    async def handle_message(client, new_message):
+        if new_message.text == "46":
+            message.reply("Поздравляю, вы человек!")
+        else:
+            message.reply("Вы даун!")
 print("Bot started!")
 app.run()
